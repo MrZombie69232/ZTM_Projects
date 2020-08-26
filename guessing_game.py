03072dfbbb725db1f97cb8c29a0dfad91e64bd88
Satyam Suman <47268048+MrZombie69232@users.noreply.github.com>
2020-08-26 11:55:20 +0530

    Add files via upload

---
+++ guessing_game.py
@@ -0,0 +1,15 @@
+from random import randint
+import sys 
+answer = randint(1,10)
+while True:
+    try:
+        guess=int(input('guess a number 1~10: '))
+        if 0<guess<11:
+            if guess== answer:
+                print('You are Lucky MF!!')
+                break
+        else:
+            print('hey dumbo, I said 1~10 ')
+    except ValueError:
+        print('Enter a number nigga')
+        continue
